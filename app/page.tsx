b160c2ad0657ba704561373997bd39efcd8bddd2
Abdur Rahman Shafi <abdur.rahman.shafi@g.bracu.ac.bd>
2025-07-17 19:30:47 +0600

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,10 @@ import { redirect } from "next/navigation"
 import { authOptions } from "@/lib/auth"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import Link from "next/link"
+
 import Homepage from "./home/page"
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-
-  if (session) {
-    redirect("/dashboard")
-  }
   return (
     <div>
       <Homepage/>
